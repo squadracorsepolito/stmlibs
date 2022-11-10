3887892e5a5077d3be7ed9d23618f796f1ee97c2
Filippo Volpe <volpx@users.noreply.github.com>
2022-11-10 03:32:14 +0100

    typo

--- longcounter/longcounter.h
+++ longcounter/longcounter.h
@@ -1,6 +1,6 @@
 /*
  * "THE BEER-WARE LICENSE" (Revision 69):
- * Squadra Corse firmware team wrote this file. As long as you retain this notice
+ * Not a Squadra Corse firmware team wrote this file. As long as you retain this notice
  * you can do whatever you want with this stuff. If we meet some day, and you 
  * think this stuff is worth it, you can buy us a beer in return.
  * 
